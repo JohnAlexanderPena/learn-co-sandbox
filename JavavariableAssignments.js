7205d0019a28ad9ff9a90db99df120ee97597cba
John Pena <jpena91@gmail.com>
2019-01-02 13:46:54 +0000

    Automatically backed up by Learn

---
+++ JavavariableAssignments.js
@@ -0,0 +1,20 @@
+ // declare the variable 
+var word = 'bird'
+
+ // assign a value to the variable
+word = 'bird' 
+
+console.log(word) // 'bird'
+
+ // assign another value to the variable
+word = 'dog'
+
+console.log(word) // 'dog'
+window.word
+
+var a = 5,
+    b = 2,
+    c = 3,
+    d = 'hello',
+    e = 'goodbye'
+    
